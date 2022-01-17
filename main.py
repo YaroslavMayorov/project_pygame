95595512cdffff655afaf962cc53088493c4808d
YaroslavMayorov <62249628+YaroslavMayorov@users.noreply.github.com>
2022-01-17 13:14:24 +0300

    Update main.py
    Added polite question

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')
     print('ok')
+    print(f'How are you {name}?')
     # Press ⌘F8 to toggle the breakpoint.
 
 
